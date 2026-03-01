fc74a6bbbf0fa76768b17caf77602f857358a2b8
Rut Birhanu <birhanurut@gmail.com>
2026-03-01 14:30:32 +0300

    feat: add initial AlbumPage component for album navigation

---
+++ Frontend/photo_album/src/app/album/[albumId]/page.tsx
@@ -0,0 +1,5 @@
+// src/app/album/[albumId]/page.tsx
+export default function AlbumPage({ params }: { params: { albumId: string } }) {
+  // fetch data or redirect
+//   return <Redirect to={`/photo/${params.albumId}`} />;
+}
